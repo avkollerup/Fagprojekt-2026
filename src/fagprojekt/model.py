d1a5ad19d89ed18de21c3797732e5540bc48b030
Julie Thanh Thanh Nguyen <julienguyen454@gmail.com>
2026-03-11 14:47:16 +0100

    Extract KV-cache

--- src/fagprojekt/model.py
+++ src/fagprojekt/model.py
@@ -1,16 +1,33 @@
-from torch import nn
+# Load model directly
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, DynamicCache
 
-class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(1, 1)
+model_id = "meta-llama/Llama-3.1-8B-Instruct"
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-if __name__ == "__main__":
-    model = Model()
-    x = torch.rand(1)
-    print(f"Output shape of model: {model(x).shape}")
+prompt = "What is 2+2?"
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+
+outputs = model.generate(
+    **inputs, 
+    max_new_tokens=50, 
+    use_cache=True, 
+    return_dict_in_generate=True,
+)
+
+# outputs.sequences contains prompts plus generated tokens
+# Print prompt and newest reply
+print(tokenizer.decode(outputs.sequences[0], skip_special_tokens=True))
+
+past_key_values = outputs.past_key_values
+generated_tokens = outputs.sequences
+
+print(past_key_values)
+print(type(past_key_values), len(past_key_values))
+
