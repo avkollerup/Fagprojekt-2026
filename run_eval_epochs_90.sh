66a24a7924c1ed426391983669fcbe64feacd665
Julie Nguyen <julienguyen454@gmail.com>
2026-06-15 17:30:59 +0200

    big evaluation run of the 6 methods

---
+++ run_eval_epochs_90.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#BSUB -J eval_epochs_90
+#BSUB -q gpua100
+#BSUB -n 4
+#BSUB -R "rusage[mem=8GB] span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 08:00
+#BSUB -o logs/eval_%J.out
+#BSUB -e logs/eval_%J.err
+
+source /work3/s245261/Fagprojekt-2026/.venv/bin/activate
+cd /work3/s245261/Fagprojekt-2026
+export NUM_EPOCHS=90
+python -u src/fagprojekt/evaluate.py
