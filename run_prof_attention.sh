916b7e523faaf7a800594e0918e796f3322e980b
avkollerup <s234136@dtu.dk>
2026-06-17 20:08:19 +0200

    batch scribts

---
+++ run_prof_attention.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#BSUB -J attention_profiling
+#BSUB -q gpua100
+#BSUB -n 4
+#BSUB -R "rusage[mem=8GB] span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 06:00
+#BSUB -o logs/attentionprofiling_%J.out
+#BSUB -e logs/attentionprofiling_%J.err
+
+source /work3/s234136/Fagprojekt-2026/.venv/bin/activate
+cd /work3/s234136/Fagprojekt-2026
+
+mkdir -p logs
+mkdir -p reports/figures/profiling
+mkdir -p models
+
+export NUM_TOKENS=300
+export LAYER_IDX=5
+export HEAD_IDX=1
+
+
+python src/profiling/atten_prof.py
