d788e557e19f53ba1d00e1d5a074209c17f2ad46
Julie Nguyen <julienguyen454@gmail.com>
2026-06-19 14:21:46 +0200

    updated batch scripts

--- run_prof_attention.sh
+++ run_prof_attention.sh
@@ -2,20 +2,20 @@
 #BSUB -J attention_profiling
 #BSUB -q gpua100
 #BSUB -n 4
-#BSUB -R "rusage[mem=8GB] span[hosts=1]"
+#BSUB -R "rusage[mem=16GB] span[hosts=1]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 06:00
+#BSUB -W 8:00
 #BSUB -o logs/attentionprofiling_%J.out
 #BSUB -e logs/attentionprofiling_%J.err
 
-source /work3/s234136/Fagprojekt-2026/.venv/bin/activate
-cd /work3/s234136/Fagprojekt-2026
+source /work3/s245261/Fagprojekt-2026/.venv/bin/activate
+cd /work3/s245261/Fagprojekt-2026
 
 mkdir -p logs
 mkdir -p reports/figures/profiling
 mkdir -p models
 
-export NUM_TOKENS=300
+export NUM_TOKENS=200
 export LAYER_IDX=5
 export HEAD_IDX=1
 
--- run_prof_inference.sh
+++ run_prof_inference.sh
@@ -2,20 +2,20 @@
 #BSUB -J prof_inference
 #BSUB -q gpua100
 #BSUB -n 4
-#BSUB -R "rusage[mem=8GB] span[hosts=1]"
+#BSUB -R "rusage[mem=16GB] span[hosts=1]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 06:00
+#BSUB -W 8:00
 #BSUB -o logs/prof_inference_%J.out
 #BSUB -e logs/prof_inference_%J.err
 
-source /work3/s234136/Fagprojekt-2026/.venv/bin/activate
-cd /work3/s234136/Fagprojekt-2026
+source /work3/s245261/Fagprojekt-2026/.venv/bin/activate
+cd /work3/s245261/Fagprojekt-2026
 
 mkdir -p logs
 mkdir -p reports/figures/profiling
 mkdir -p models
 
-export NUM_TOKENS=300
+export NUM_TOKENS=200
 export LAYER_IDX=5
 export HEAD_IDX=1
 
