dd9f6ed6fa58fe75a27548da8ac0fe0272109d76
Julie Nguyen <julienguyen454@gmail.com>
2026-06-19 16:39:05 +0200

    merge attention profs back into one file. New results

--- src/profiling/attention_prof.py
+++ src/profiling/attention_prof.py
@@ -3,9 +3,10 @@ from fagprojekt.model import load_model, get_messages, get_kvq
 from fagprojekt.SVD import method_1, method_2, method_3, method_4
 from fagprojekt.K_means import k_means_clustering
 from fagprojekt.SVD_Nystrom import nystrom_attention_approx
+from fagprojekt.Hokus_pokus import hokus_pokus, build_mlp
 
 import torch
-from torch.profiler import profile, ProfilerActivity
+from torch.profiler import profile, ProfilerActivity, record_function
 from torch.utils.flop_counter import FlopCounterMode
 
 
@@ -14,7 +15,7 @@ def SVD1(key_head, query_head, value_head, num_tokens, layer_idx, head_idx, k, i
         with FlopCounterMode(display=False) as flop_counter:
             result = method_1(key_head, query_head, value_head, k=k)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Method 1 FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/method_1_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_k_{k}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -26,7 +27,7 @@ def SVD2(key_head, query_head, value_head, num_tokens, layer_idx, head_idx, k, i
         with FlopCounterMode(display=False) as flop_counter:
             result = method_2(key_head, query_head, value_head, k=k)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Method 2 FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/method_2_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_k_{k}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -38,7 +39,7 @@ def SVD3(key_head, query_head, value_head, num_tokens, layer_idx, head_idx, k, i
         with FlopCounterMode(display=False) as flop_counter:
             result = method_3(key_head, query_head, value_head, k=k)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Method 3 FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/method_3_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_k_{k}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -50,7 +51,7 @@ def SVD4(key_head, query_head, value_head, num_tokens, layer_idx, head_idx, k, i
         with FlopCounterMode(display=False) as flop_counter:
             result = method_4(key_head, query_head, value_head, k=k)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Method 4 FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/method_4_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_k_{k}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -62,7 +63,7 @@ def Nystrom_Attention(query_head, value_head, key_head, k, num_tokens, layer_idx
         with FlopCounterMode(display=False) as flop_counter:
             result = nystrom_attention_approx(key_head, query_head, value_head, k=k)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Nystrom Attention FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/nystrom_attention_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_k_{k}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -74,7 +75,7 @@ def flash_attention(query_head, value_head, key_head, num_tokens, layer_idx, hea
         with FlopCounterMode(display=False) as flop_counter:
             result = torch.nn.functional.scaled_dot_product_attention(query_head, key_head, value_head, is_causal=True)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Flash Attention FLOP {i} :  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/flash_attention_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -90,7 +91,7 @@ def naive_attention(query_head, value_head, key_head, num_tokens, layer_idx, hea
             attn_weights = torch.nn.functional.softmax(M + attn_scores, dim=-1)
             result = torch.matmul(attn_weights, value_head)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'Naive Attention FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/naive_attention_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
@@ -102,9 +103,36 @@ def KMeans_Attention(query_head, value_head, key_head, num_tokens, layer_idx, he
         with FlopCounterMode(display=False) as flop_counter:
             result = k_means_clustering(key_head, value_head, query_head, clusters=clusters)
         torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
-    with open(f"reports/figures/profiling/flops_svd_nystrom_kmeans.txt", "a") as f:
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
         f.write(f'K-means FLOP {i}:  {flop_counter.get_total_flops()} \n')
     with open(f"reports/figures/profiling/kmeans_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_clusters_{clusters}.txt", "a") as f:
+        f.write(f"--- Iteration {i} ---\n")
+        f.write(prof.key_averages().table(sort_by="self_cpu_time_total"))
+        f.write("\n\n")
+
+
+def load_g_theta(k):
+    method = "mlp"
+    num_epochs = 90
+
+    # Load saved model:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_path = f"models/g_theta_weights_{method}_k_{k}_epochs_{num_epochs}.pth"
+    g_theta = build_mlp(k).to(device)
+    g_theta.load_state_dict(torch.load(model_path, map_location=device))
+    g_theta.eval()
+    return g_theta
+
+
+def Hokus_Pokus(query_head, value_head, key_head, method, layer_idx, head_idx, k, num_tokens, g_theta, i):
+    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], profile_memory=True, record_shapes=True) as prof:
+        with FlopCounterMode(display=False) as flop_counter:
+            with record_function("Hokus Pokus attention"):
+                result = hokus_pokus(query_head, value_head, key_head, k, method, g_theta)
+        torch.cuda.synchronize()  # Ensure all CUDA operations are finished before stopping the profiler
+    with open(f"reports/figures/profiling/flops.txt", "a") as f:
+        f.write(f'Hokus Pokus FLOP {i}:  {flop_counter.get_total_flops()} \n')
+    with open(f"reports/figures/profiling/Hokus_Pokus_layer_{layer_idx}_head_{head_idx}_tokens_{num_tokens}_k_{k}.txt", "a") as f:
         f.write(f"--- Iteration {i} ---\n")
         f.write(prof.key_averages().table(sort_by="self_cpu_time_total"))
         f.write("\n\n")
@@ -122,6 +150,9 @@ if __name__ == "__main__":
 
     model, tokenizer = load_model(want_print=False)
 
+    # Load trained g_theta:
+    g_theta = load_g_theta(k)
+
     for i in range(11):
         messages, _, _ = get_messages(f"document-haystack/AIG/AIG_25Pages/Text_TextNeedles/AIG_25Pages_TextNeedles_page_{i+1}.txt", num_tokens=num_tokens)
         key_head, value_head, query_head = get_kvq(model=model, tokenizer=tokenizer, messages=messages, layer_idx=layer_idx, head_idx=head_idx)
@@ -137,3 +168,5 @@ if __name__ == "__main__":
         naive_attention(query_head=query_head, value_head=value_head, key_head=key_head, num_tokens=num_tokens, layer_idx=layer_idx, head_idx=head_idx, i=i)
 
         KMeans_Attention(query_head=query_head, value_head=value_head, key_head=key_head, num_tokens=num_tokens, layer_idx=layer_idx, head_idx=head_idx, clusters=clusters, i=i)
+
+        Hokus_Pokus(query_head=query_head, value_head=value_head, key_head=key_head, method="mlp", layer_idx=layer_idx, head_idx=head_idx, k=k, num_tokens=num_tokens, g_theta=g_theta, i=i)
